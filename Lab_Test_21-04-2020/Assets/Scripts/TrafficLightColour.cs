9165adf0973579f49ca7a2b6e28bed3ebfa23a53
Zeppelin03 <56638997+Zeppelin03@users.noreply.github.com>
2020-04-21 14:28:26 +0100

    Worning traffic lights

---
+++ Lab_Test_21-04-2020/Assets/Scripts/TrafficLightColour.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLightColour : MonoBehaviour
+{
+
+    public State state;
+    public float timer = 0f;
+
+    public enum State
+    {
+        yellow,
+        red,
+        green,
+    }
+
+    void Awake()
+    {
+        var ObjRenderer = GetComponent<Renderer>();
+
+        int randomState = Random.Range(0, 3);
+        switch (randomState)
+        {
+            case 0:
+                ObjRenderer.material.SetColor("_Color", Color.yellow);
+                state = State.yellow;
+                timer = 4f;
+                break;
+
+            case 2:
+                ObjRenderer.material.SetColor("_Color", Color.red);
+                state = State.red;
+                timer = Random.Range(5f, 10f);
+                break;
+
+            case 1:
+                ObjRenderer.material.SetColor("_Color", Color.green);
+                state = State.green;
+                timer = Random.Range(5f, 10f);
+                break;
+        }
+    }
+
+    void Update()
+    {
+        ChangeState();
+    }
+
+    void ChangeState()
+    {
+        var ObjRenderer = GetComponent<Renderer>();
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            switch (state)
+            {
+                case State.yellow:
+                    state = State.red;
+                    ObjRenderer.material.SetColor("_Color", Color.red);
+                    timer = Random.Range(5f, 10f);
+                    break;
+
+                case State.red:
+                    state = State.green;
+                    ObjRenderer.material.SetColor("_Color", Color.green);
+                    timer = Random.Range(5f, 10f);
+                    break;
+
+                case State.green:
+                    state = State.yellow;
+                    ObjRenderer.material.SetColor("_Color", Color.yellow);
+                    timer = 4f;
+                    break;
+            }
+        }
+    }
+}
---
+++ Lab_Test_21-04-2020/Assets/Scripts/TrafficLightScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLightScript : MonoBehaviour
+{
+    public List<GameObject> trafficlights;
+    public float radius = 10;
+    public int lightsAmount = 10;
+
+    void Start()
+    {
+        TrafficLightsSpawn();
+    }
+
+    void TrafficLightsSpawn()
+    {
+        trafficlights.Clear();
+        float thetaInc = (Mathf.PI * 2) / lightsAmount;
+        for (int i = 0; i < lightsAmount; i++)
+        {
+            float theta = i * thetaInc;
+            Vector3 pos = new Vector3(Mathf.Sin(theta) * radius, 0, Mathf.Cos(theta) * radius);
+            GameObject Cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+            Cylinder.transform.position = pos;
+            Cylinder.name = "Trafficlight " + (i + 1);
+            trafficlights.Add(Cylinder);
+            Cylinder.gameObject.AddComponent<TrafficLightColour>();
+            Cylinder.transform.parent = this.transform;
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        float thetaInc = (Mathf.PI * 2) / lightsAmount;
+        for (int i = 0; i < lightsAmount; i++)
+        {
+            float theta = i * thetaInc;
+            Vector3 pos = new Vector3(Mathf.Sin(theta) * radius, 0, Mathf.Cos(theta) * radius);
+
+            Gizmos.DrawWireSphere(pos, 2);
+        }
+    }
+}
